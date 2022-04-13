e342f86a408109dddeb0081017edf8107fcbac2f
RomanAnpilov <rebokuter@gmail.com>
2022-04-13 20:09:17 +0300

    Initial Commit

---
+++ NetflixApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NetflixApp
+//
+//  Created by Роман Анпилов on 13.04.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
